ca9882bda3bc3c78d8427cbe778983b3f5f29419
eledupown <eduardo.galvez@mail.udp.cl>
2022-05-04 00:22:58 -0400

    running

--- Tarea1/grpc/grpc_client/index.js
+++ Tarea1/grpc/grpc_client/index.js
@@ -1,11 +1,33 @@
 const express=require("express")
 const cors= require("cors")
 const client= require("./grpc_client")
+const redis = require('redis')
 
 const app= express()
+const redisClient = redis.createClient({
+    host: 'cache',   
+    // port: 6379
+});
 
-app.get('/:name?', (req, res)=>{
-    client.getItem({name:req.params.name}, (_,data)=>{res.json(data)})
-    
+redisClient.on("error", function(error) {
+    console.error(error);
+});
+
+app.get('/:name?', async (req, res)=>{
+    redisClient.get(req.params.name || "", (err, data)=>{
+        if(err){
+            client.getItem({name:req.params.name}, async (_,data)=>{
+                await redisClient.set(req.params.name || "", JSON.stringify(data))
+                res.json(data)})
+        }else{
+            if(!data){
+                client.getItem({name:req.params.name}, async (_,data)=>{
+                    await redisClient.set(req.params.name || "", JSON.stringify(data))
+                    res.json(data)})
+            }else{
+                res.json(data)
+            }
+        }
+    })
 })
 app.listen(3000)
