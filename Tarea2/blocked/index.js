4563fefb195877ceeef7af0fec078f588bbcd2e9
eledupown <eduardo.galvez@mail.udp.cl>
2022-05-23 17:57:33 -0400

    aaaa

--- Tarea2/blocked/index.js
+++ Tarea2/blocked/index.js
@@ -6,11 +6,16 @@ app.use(express.json());
 
 const consumer = kafka.consumer({ groupId: "security-group" });
 
-
-app.get("/blocked", async function (req, res) {
+app.get("/bloq", async function (req, res) {
     const bloqFile = await fs.readFile("/bloqUsers.json", "utf-8");
     const bloqUsers = JSON.parse(bloqFile);
     res.json({
       "users-blocked": bloqUsers,
     });
-  });
+});
+
+app.listen(5000, async function() {
+    await consumer.connect();
+    await consumer.subscribe({ topic: "login", fromBeginning: true });
+    console.log("listening in port 5000");
+});
